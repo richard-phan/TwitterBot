dfa284484b3d174adf48b7401b5fb796d0ca5cb9
Richard <riichardphan@gmail.com>
2020-12-02 10:25:10 -0800

    add tweeting functionality

--- twitter.py
+++ twitter.py
@@ -1,11 +1,41 @@
 import tweepy
 import os
+import urllib.request
+import json
 
 class StreamListener(tweepy.StreamListener):
     
-    def on_status(self, status):
-        print(status.text)
+    def send_tweet(self, data, msg):
+        with urllib.request.urlopen('https://api.covidtracking.com/v1/us/daily.json') as request:
+            covid_data = json.loads(request.read().decode())
 
+            days_ago = 0
+            if len(msg) > 1:
+                days_ago = int(msg[1])
+
+            stats = covid_data[days_ago]
+            raw_date = str(stats['date'])
+            date = raw_date[:4] + '-' + raw_date[4:6] + '-' + raw_date[6:]
+
+            api.update_status(
+                'COVID STATISTICS'
+                + '\n--------------------'
+                + '\nDate: ' + date 
+                + '\nConfirmed cases: ' + str(stats['positive'])
+                + '\nNew cases: ' + str(stats['positiveIncrease'])
+                + '\nDeaths: ' + str(stats['death'])
+                + '\nNew deaths: ' + str(stats['deathIncrease'])
+                + '\nCurrently hospitalized: ' + str(stats['hospitalizedCurrently'])
+                + '\nTotal hospitalized: ' + str(stats['hospitalized']),
+                in_reply_to_status_id=data['id']
+            )
+
+    def on_status(self, status):
+        data = status._json
+        
+        msg = data['text'].split(' ')
+
+        self.send_tweet(data, msg)
 
 auth = tweepy.OAuthHandler(consumer_key=os.getenv('CONSUMER_KEY'),
                            consumer_secret=os.getenv('CONSUMER_SECRET'))
@@ -17,4 +47,4 @@ api = tweepy.API(auth)
 streamListener = StreamListener()
 stream = tweepy.Stream(auth=api.auth, listener=streamListener)
 
-stream.filter(track=['@Richard48887283'])
+stream.filter(track=['@' + api.me().screen_name])
